b2338061d696e8c7b4316f8cd56d8ffcad0ebeee
Marko Gaćeša <markogacesa@hotmail.com>
2025-11-09 14:03:39 +0100

    add LICENSE

--- udpstar/message/ping/parse_test.go
+++ udpstar/message/ping/parse_test.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2024, 2025 by Marko Gaćeša
+// Copyright (c) 2024, 2025 by Marko Gaćeša.
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file or http://www.apache.org/licenses/LICENSE-2.0 for details.
 
 package ping
 
--- udpstar/message/token.go
+++ udpstar/message/token.go
@@ -1,4 +1,6 @@
-// Copyright (c) 2023,2024 by Marko Gaćeša
+// Copyright (c) 2023,2024 by Marko Gaćeša.
+// Licensed under the Apache License, Version 2.0.
+// See the LICENSE file or http://www.apache.org/licenses/LICENSE-2.0 for details.
 
 package message
 
